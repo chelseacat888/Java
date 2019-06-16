0aa391cd303789869eb33785e59b973030080227
chelseacat888 <43200378+chelseacat888@users.noreply.github.com>
2019-06-16 14:00:59 -0500

    face I made with Java

---
+++ Emotion.java
@@ -0,0 +1,19 @@
+/* This Program prints a face
+ * Chelsea Smith 
+ * 06/15/2019
+ * JDK used
+ */
+public class Emotion {
+
+    public static void main(String[] args) {
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("        *      ");
+        System.out.println("          *    ");
+        System.out.println("        *      ");
+        System.out.println("*              *");
+        System.out.println(" *            *");
+        System.out.println("  ************ ");
+    }
+
+}
